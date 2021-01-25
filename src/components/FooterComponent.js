b2ccbc806735f9bb5d8575bffb7563bfdc50fe04
MIRUNA POP <mirunapop2002@gmail.com>
2021-01-25 20:50:59 +0200

    upload to git

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,10 +8,10 @@ function Footer(props) {
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
                     <ul className="list-unstyled">
-                        <li><Link to="/home">Home</Link></li>
-                        <li><Link to="/aboutus">About</Link></li>
-                        <li><Link to="/menu">Menu</Link></li>
-                        <li><Link to="/contactus">Contact</Link></li>
+                        <li ><Link to="/home" className="link">Home</Link></li>
+                        <li><Link to="/aboutus" className="link">About</Link></li>
+                        <li><Link to="/menu" className="link">Menu</Link></li>
+                        <li><Link to="/contactus" className="link">Contact</Link></li>
                     </ul>
                 </div>
                 <div className="col-7 col-sm-5">
@@ -22,7 +22,7 @@ function Footer(props) {
 		              HONG KONG<br />
 		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
 		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+		              <i className="fa fa-envelope fa-lg"></i>: <a className="link" href="mailto:confusion@food.net">
                          confusion@food.net</a>
                     </address>
                 </div>
@@ -33,13 +33,12 @@ function Footer(props) {
                         <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
                         <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                        <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
                     </div>
                 </div>
             </div>
             <div className="row justify-content-center">             
                 <div className="col-auto">
-                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                    <p>© Copyright 2021 Alberto Ristorante Italiano</p>
                 </div>
             </div>
         </div>
